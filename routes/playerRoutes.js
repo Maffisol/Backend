85a84758aab0feffc403e5a051d80301e7275b4a
maffisol <m.ertemel.91@gmail.com>
2025-01-12 16:25:21 +0100

    Updated README file

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -511,4 +511,22 @@ router.get('/by-username/:username', async (req, res) => {
     }
 });
 
+//guide//
+router.post('/update-guide-status', async (req, res) => {
+    const { userId, hasSeenGuide } = req.body;
+    
+    try {
+        // Zoek de speler op basis van 'userId' en werk de 'hasSeenGuide' status bij
+        const user = await Player.findByIdAndUpdate(userId, { hasSeenGuide }, { new: true });
+
+        // Verstuur de bijgewerkte spelerstatus via Socket.IO naar de frontend (als je dit wenst)
+        req.app.get('io').emit('guideStatusUpdated', { userId, hasSeenGuide });
+
+        res.status(200).json(user); // Stuur de bijgewerkte speler terug als reactie
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating guide status', error });
+    }
+});
+    
+
 module.exports = router;
